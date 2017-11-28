5db5e5d8ae23843558ebe7c15c1050ffebc83c1a
Adrianna Sliwak <adri1688@edu.easj.dk>
2017-11-28 10:01:00 +0100

    ....

---
+++ FCalendar/Model/Host.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCalendar.Model
+{
+    class Host
+    {
+        string CPR;
+        string Name;
+        string Mail;
+        string TempPassword;
+        string HashedPassword;
+        string AdminNotes;
+        bool IsVerified;
+        int VerifiedEvent;
+        bool IsRecomendet;
+        string ShortDescription;
+        
+        public Host(string CPR, string Name, string Mail, string password)
+        {
+
+        }
+        public Host()
+        {
+            HashedPassword = TempPassword;
+        }
+    }
+}
