a17c3142b527d7dce96c3fbcf8159b95694ba160
Ana-Maria Cretescu <anax1030@edu.easj.dk>
2017-11-30 00:12:16 +0100

    map on

---
+++ FCalendar/Model/CoordinateConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace FCalendar.Model
+{      
+  
+        public static class CoordinateConverter
+        {
+            public static GeoCoordinate ConvertGeocoordinate(Geocoordinate geocoordinate)
+            {
+                return new GeoCoordinate
+                (
+                    geocoordinate.Latitude,
+                    geocoordinate.Longitude
+                    
+                );
+            }
+
+            
+
+        }
+
+    public class GeoCoordinate
+    {
+        public GeoCoordinate(double geocoordinateLatitude, double geocoordinateLongitude)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
